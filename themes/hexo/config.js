41d011ebccd4b12c179069d2b644e469b66e5257
wasd <2661983783@qq.com>
2025-05-25 19:08:43 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -27,11 +27,11 @@ const CONFIG = {
   HEXO_POST_LIST_PREVIEW: false, // 读取文章预览
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
-  HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
+  HEXO_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
   HEXO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
   HEXO_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
-  HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
+  HEXO_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: false, // 跳到评论区
